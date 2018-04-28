d17d81cce84302847651b13bb6ac559c31c508d4
Kristaps622 <k.kusins@avanade.com>
2018-04-28 04:34:39 +0300

    Updates

--- server.js
+++ server.js
@@ -1,92 +1,143 @@
+
 var Discord = require('discord.io');
 
+
 var bot = new Discord.Client({
-    token: "NDE5MjgxMTY1MzI1Njk3MDQ0.DXt1kg.EevC2AWwT1_i7Uv9tlaMUI54GM8",
+    token: "",
     autorun: true
 });
-
-var messages = []
-var members = []
 
 bot.on('ready', function() {
     console.log('Logged in as %s - %s\n', bot.username, bot.id);
 });
 
+var allowedChannel = "431461850836893696"
+var playerDB = []
+
+//utils
+function checkPlayerInDB(userID) {
+  return playerDB.find(function(player){return player.userID === userID})
+}
+
+function sendPlayerStats(userID){
+  playerDB.map( function(item) {
+    if(item.userID === userID) {
+      var playerStats = "``` =========== Character Stats ===========\n Health: "+item.stats.health+"\n Level: "+item.stats.level+"\n XP: "+item.stats.xp+"/"+item.stats.level*1000+"\n Attack: "+item.stats.attack+"\n Defense: "+item.stats.defense+"\n Money: "+item.stats.money+"\n Kills: "+item.stats.kills+"\n Deaths: "+item.stats.deaths+"```"
+      botSendPrivateMessage(playerStats, userID)
+    }
+  })
+}
+
+function attemptGainXpAndMoney(userID) {
+  if(Math.floor((Math.random() * 3) + 1) === 1){
+
+    playerDB.map(function(player){
+
+      if(userID === player.userID) {
+
+        var xpGained = Math.floor((Math.random() * 500) + 1)
+        var moneyGained = Math.floor((Math.random() * 50) + 1)
+
+        player.stats.xp = player.stats.xp + xpGained
+
+        if(player.stats.xp >= 1000*player.stats.level) {
+          var xpLeftOver = player.stats.xp - 1000*player.stats.level
+          player.stats.xp = xpLeftOver
+          player.stats.level++
+          player.stats.health = 10*player.stats.level
+          botSendPrivateMessage('Congratulations you have leveled up! Your level is: '+player.stats.level, userID)
+        }
+
+        player.stats.money = player.stats.money + moneyGained
+
+        console.log("------------------------------------\nplayer.userID: "+player.userID+"\n\n userID: "+userID+"\n-------\n"+player.stats+"\n\n\n\n" )
+      }
+
+    })
+
+  }
+}
+//utils
+
+function botSendMessage(message) {
+  bot.sendMessage({
+    to: allowedChannel,
+    message: message
+  })
+}
+
+function botSendPrivateMessage(message, userID) {
+  bot.sendMessage({
+    to: userID,
+    message: message
+  })
+}
+
+function botDeleteMessage(messageID) {
+  bot.deleteMessage({
+    channelID: allowedChannel,
+    messageID: messageID
+  })
+}
+
 bot.on('message', function(user, userID, channelID, message, event) {
-    if (message) {
-		console.log(user + ": " + message)
+    if(checkPlayerInDB(userID)){attemptGainXpAndMoney(userID)}
+
+    if (allowedChannel === channelID && message === '!register') {
+      if(checkPlayerInDB(userID)){
+        botSendMessage("You already have an account!")
+        sendPlayerStats(userID)
+      }else {
+      botSendMessage("Player registered!")
+      playerDB.push({userID, user, stats: {
+        health: 10,
+        xp: 0,
+        level: 1,
+        money: 0,
+        attack: 0,
+        defense: 0,
+        kills: 0,
+        deaths: 0,
+      }})
+      console.log(playerDB)
+      }
+    }
+
+    if(allowedChannel === channelID && message === '!mystats') {
+      sendPlayerStats(userID)
+    }
+
+    if(allowedChannel === channelID && message === '!topMoney'){
+      var sortedPlayers = playerDB.sort(function (a, b) {
+        return a.stats.money - b.stats.money;
+      })
+
+      sortedPlayers.slice(0,5)
+      sortedPlayers.reverse()
+
+      var topPlayerString = '```---- Player Money Top ----\n'
+      sortedPlayers.map(function(player, index){
+        index++
+        topPlayerString = topPlayerString + '\n '+index+': '+player.user+' - '+player.stats.money+'$'
+      })
+      topPlayerString = topPlayerString+'\n----------------```'
+      botSendMessage(topPlayerString)
+    }
+
+    if(allowedChannel === channelID && message === '!topLevel'){
+      var sortedPlayers = playerDB.sort(function (a, b) {
+        return a.stats.level - b.stats.level;
+      })
+
+      sortedPlayers.slice(0,5)
+      sortedPlayers.reverse()
+
+      var topPlayerString = '```---- Player Level Top ----\n'
+      sortedPlayers.map(function(player, index){
+        index++
+        topPlayerString = topPlayerString + '\n '+index+': '+player.user+' - '+player.stats.level
+      })
+      topPlayerString = topPlayerString+'\n----------------```'
+      botSendMessage(topPlayerString)
     }
 });
-
-bot.on('message', function(user, userID, channelID, message, event){
-	if(message === "!help") {
-		bot.sendMessage({
-			to: channelID,
-			message: "Commands: \n\n !sub - You will be notified about any deleted messages. \n !unsub - You will be removed from the list where you get notified about any messages \n\n End of transmission..."
-		})
-	}
-	if(message === "!sub"){
-		bot.sendMessage({
-			to: channelID,
-			message: "Gotcha Boss!"
-		})
-		members.push({'userID': userID})
-	}
-	if(message === "!unsub"){
-		bot.sendMessage({
-			to: channelID,
-			message: "Done, removed ya from the list!"
-		})
-		members.map(function(member, index) {
-			if(member.userID === userID) {
-				members.splice(index, 1)
-			}
-		})
-	}
-})
-
-bot.on('any', function(event) {
-	if(event && event.t === "MESSAGE_DELETE") {
-		messages.map( function(message) {
-			if(message.id === event.d.id) {
-				console.log( '------------- DELTETED MESSAGE -----------')
-				console.log(message.message)
-				console.log(message.attachment)
-				console.log(' ------------------------------------------')
-				members.map(function(member){
-					bot.sendMessage({
-							to: member.userID,
-							message: 'Message was deleted on Exile.lv: \n ```User who deleted the message: '+ event.d.id + '\n Original message author: '+ message.author + '\n Message: ' + message.message+'```'
-					})
-				})
-			}
-		})
-	}
-	
-	if(event && event.t === "MESSAGE_CREATE") {
-		if(event.d.content != "!list_messages" && event.d.content != "!list" && event.d.content != "!subscribe" && event.d.content != "!unsubscribe" && event.d.content != "!help")
-		messages.push({'id': event.d.id, 'author': event.d.author.username , 'message': event.d.content, 'attachment': event.d.attachments})
-	}
-	
-	if(messages.length > 1001) {
-		messages.shift()
-	}
-});
-
-bot.on('message', function(user, userID, channelID, message, event){
-	if(message === "!list_messages") {
-		var listOfMessages = messages.map(function(item){
-			return bot.sendMessage({
-				to: userID,
-				message: "--------\n Username: "+ item.user +"\n Message: " + item.message + "\n Attachments: "+ item.attachment+"\n --------\n"
-			})
-		});
-
-	}
-});
-
-bot.on('any', function(event){
-	if(event.t === "TYPING_START") {
-
-	}
-})

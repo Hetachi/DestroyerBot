9cc917326936f98207bd945a3bbc9bbd986c7d7a
Kristaps <Hetachi@users.noreply.github.com>
2018-11-16 03:03:14 +0200

    Update botToken.js

--- components/config/botToken.js
+++ components/config/botToken.js
@@ -1,2 +1,2 @@
 // Place your personal bot token in here
-module.exports = "NDMxNDkzMzIwMzk3ODE1ODA4.Ds9rFw.zTV2TgVLvEqdFu5KjzimUFo-ok0" //GET YOUR DISCORD BOT API TOKEN HERE: https://discordapp.com/login?redirect_to=%2Fdevelopers%2Fapplications%2F
+module.exports = "" //GET YOUR DISCORD BOT API TOKEN HERE: https://discordapp.com/login?redirect_to=%2Fdevelopers%2Fapplications%2F
